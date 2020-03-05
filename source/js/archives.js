f8076c8353ebbd7715d4a79d1c7815d737627b23
zombez <alexey-k79@yandex.ru>
2020-03-05 16:12:18 +0300

    работа с архивом в верстке и js

---
+++ source/js/archives.js
@@ -0,0 +1,11 @@
+function() {
+	let dropown = document.getElementById( 'archives' );
+	function onSelectChange() {
+		if ( dropown.options[
+			dropown.selectedIndex ].value !== '' ) {
+			document.location.href =
+			this.options[ this.selectedIndex ].value;
+		}
+	}
+	dropown.onchange = onSelectChange;
+};
