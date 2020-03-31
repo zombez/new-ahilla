50ba8946e42ece09dfd216456924be83227910f2
zombez <alexey-k79@yandex.ru>
2020-03-31 10:03:56 +0300

    js для модалки

--- source/js/common.js
+++ source/js/common.js
@@ -105,3 +105,23 @@ var blazy = new Blazy({
   selector: 'img',
   offset: 1000
 });
+
+// модальное окно
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("form-social__close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
